0d10bc4517858fbcafd9af1016faecd409620957
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-05-03 16:30:36 +0300

    feat(ng-poc): add theming

--- Test Reveal_angular/src/app/view1/view1.component.ts
+++ Test Reveal_angular/src/app/view1/view1.component.ts
@@ -10,10 +10,27 @@ export class View1Component {
   @ViewChild('revealView')
   public el!: ElementRef;
 
+  private setRevealTheme() {
+    const style = window.getComputedStyle(this.el.nativeElement);
+    const theme = new $.ig.RevealTheme();
+
+    theme.regularFont = style.getPropertyValue('--ig-font-family');
+    theme.mediumFont = theme.regularFont;
+    theme.boldFont = theme.regularFont;
+
+    theme.fontColor = style.getPropertyValue('--ig-surface-500-contrast');
+    theme.isDark = theme.fontColor !== 'black';
+    theme.dashboardBackgroundColor = `hsl(${style.getPropertyValue('--ig-gray-100')})`;
+    theme.visualizationBackgroundColor = `hsl(${style.getPropertyValue('--ig-surface-500')})`;
+
+    $.ig.RevealSdkSettings.theme = theme;
+  }
+
   public ngAfterViewInit(): void {
+    this.setRevealTheme();
     $.ig.RevealSdkSettings.setBaseUrl("https://samples.revealbi.io/upmedia-backend/reveal-api/");
     $.ig.RVDashboard.loadDashboard("Sales", (dashboard) => {
-      var revealView = new $.ig.RevealView("#revealView");
+      var revealView = new $.ig.RevealView(this.el.nativeElement);
       revealView.dashboard = dashboard;
 
       var revealView1 = new $.ig.RevealView("#revealView1");
